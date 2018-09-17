a7a3407456ac617dd962ff167f34192b479a56f7
Chad Woolley <thewoolleyman@gmail.com>
2018-09-17 16:53:53 -0600

    add some ActiveRecord/db tracing
    Signed-off-by: Matthew Conger-Eldeen <mcongereldeen@pivotal.io>
    Signed-off-by: Chad Woolley <cwoolley@pivotal.io>

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # Instrument all activerecord actions for tracing
+
+  around_save do |activity, block|
+    with_span("ActiveRecord#save #{activity.class.name}") do
+      block.call
+    end
+  end
+
+  around_destroy do |activity, block|
+    with_span("ActiveRecord#destroy #{activity.class.name}") do
+      block.call
+    end
+  end
 end
